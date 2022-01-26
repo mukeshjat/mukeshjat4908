7bd3064d326162e4c31b4dcee7dfb76dabbbd8e8
Mukesh Jat <mukeshjat3355@gmail.com>
2022-01-26 20:30:05 +0530

    main

--- assest/js/scrpit.js
+++ assest/js/scrpit.js
@@ -11,15 +11,15 @@ function menubaropen(){
   function myFunction() {
 var dots = document.getElementById("dots");
 var moreText = document.getElementById("more");
-var btnText = document.getElementsByClassName("myBtn");
+var btnText = document.getElementById("myBtn");
 
 if (dots.style.display === "none") {
   dots.style.display = "inline";
-  btnText.innerHTML = "Read less";
+  btnText.innerText = "Read more";
   moreText.style.display = "none";
 } else {
   dots.style.display = "none";
-  btnText.innerHTML = "Read less";
+  btnText.innerText = "Read less";
   moreText.style.display = "inline";
 }
 }
